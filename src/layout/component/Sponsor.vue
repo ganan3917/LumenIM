d33d3e2afdf403c9cd32bc9aa24641282ff7f6a7
game760 <134137519+game760@users.noreply.github.com>
2026-01-11 11:17:52 +0800

    修改赞助

--- src/layout/component/Sponsor.vue
+++ src/layout/component/Sponsor.vue
@@ -22,28 +22,7 @@ const onClose = () => {
 
 <template>
   <div>
-    <div class="reward" v-show="isShow">
-      <div class="title">
-        <span>开源支持</span>
-        <n-icon :size="18" :component="Close" @click="onClose" />
-      </div>
-
-      <div class="main">
-        <div class="pay-box">
-          <img
-            src="https://im.gzydong.com/public/media/image/talk/20230226/dfc440a2bdb2ae852d57a2003b3b350b_737x742.png"
-          />
-          <p>支付宝</p>
-        </div>
-        <div class="pay-box">
-          <img
-            src="https://im.gzydong.com/public/media/image/talk/20230226/6493e01b44b6e5cb6a219cea863eb13e_621x620.png"
-          />
-          <p>微信</p>
-        </div>
-      </div>
-      <div class="footer">开源不易，如果你觉得项目对你有帮助，可以请作者喝杯咖啡☕️！鼓励下...</div>
-    </div>
+    
   </div>
 </template>
 <style lang="less" scoped>
