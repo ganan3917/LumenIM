99907db15f00de9aed8185ab0ba85529085e6de0
game760 <134137519+game760@users.noreply.github.com>
2026-01-12 10:38:04 +0800

    更新文字

--- src/hooks/useAccessPrompt.ts
+++ src/hooks/useAccessPrompt.ts
@@ -2,8 +2,8 @@ export const useAccessPrompt = () => {
   // 3秒后获取用户浏览器权限
   setTimeout(() => {
     window['$notification'].create({
-      title: '友情提示',
-      content: '此站点仅供演示、学习所用，请勿上传或发布违法信息。',
+      title: '友情提醒',
+      content: '此站作为游戏粉丝和爱好者学习、交流之用，请勿上传或发布违法信息，包括但不限于文字、图片等。网络非违法之地，请遵守法律法规及平台社区规范。文明交流，理性发言，共同维护健康的网络沟通环境。',
       duration: 30000
     })
   }, 3000)
