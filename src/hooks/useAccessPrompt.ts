d9f03cac25c0c9e74d809328e0201aa9a3b9ec83
game760 <134137519+game760@users.noreply.github.com>
2026-01-12 10:16:09 +0800

    修改提示

--- src/hooks/useAccessPrompt.ts
+++ src/hooks/useAccessPrompt.ts
@@ -3,7 +3,7 @@ export const useAccessPrompt = () => {
   setTimeout(() => {
     window['$notification'].create({
       title: '友情提示',
-      content: '此站点仅供演示、学习所用，请勿进行非法操作、上传或发布违法资讯。',
+      content: '此站点仅供演示、学习所用，请勿上传或发布违法信息。',
       duration: 30000
     })
   }, 3000)
