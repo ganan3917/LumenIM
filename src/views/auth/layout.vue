065827e7e15d780cc722c24e4c33b95f9c4a4313
game760 <134137519+game760@users.noreply.github.com>
2026-01-12 11:20:20 +0800

    修改文字

--- src/views/auth/layout.vue
+++ src/views/auth/layout.vue
@@ -10,7 +10,6 @@ import { isElectronMode } from '@/utils/electron'
   </section>
 
   <div class="copyright">
-    <span>©2022 - 2026 </span> <br/>
     <span>管理账号：15511112222</span> <br/>
     <span>管理邮箱：diwangkeji#outlook.com</span> <br/>
     <span><a href="http://service.queryip.top/guifan.html" target="_blank">社区规范</a></span>
